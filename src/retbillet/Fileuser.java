c4768d448e3825d3847558ad79133a8eb6240bbb
phillipbomholtz <phillip.bomholtz@gmail.com>
2019-03-16 12:28:31 +0100

    Fileuser search update

--- src/retbillet/Fileuser.java
+++ src/retbillet/Fileuser.java
@@ -6,6 +6,7 @@ import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.File;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -15,6 +16,7 @@ public class Fileuser  {
     private String filename;
     private String path;
     private boolean filecheck = true;
+    private String searchstring;
    
    
     // basic constructer that assigns filename and path
@@ -75,11 +77,31 @@ public class Fileuser  {
        }
        return ifempty;   
        }else{
-       System.out.println("ERROR: file not to be accesed! behaves as if empty empty");
+       System.out.println("ERROR: file not to be accesed! behaves as if empty");
        return true;
        }
    }
-   /*public String fileSearch(String s){
-      mm 
-   }*/
+   public int fileSearchUser(String USERNAME, String PASSWORD)throws IOException{
+      BufferedReader search = new BufferedReader(new FileReader(path+filename));
+      String tempUser;
+      String tempPass;
+      searchstring = search.readLine();
+      while(searchstring != null){
+          StringTokenizer tok = new StringTokenizer(searchstring,"(,)");
+          tempUser = tok.nextToken();
+          tempPass = tok.nextToken();
+          if(tempUser.equals(USERNAME)){
+           if(tempPass.equals(PASSWORD)){
+               return 1;
+           }
+           return 0;
+       }
+         searchstring = search.readLine();
+      }
+       return -1;
+   }
+   
+   public int userBalance(){
+       return 0;
+   }
 }
--- src/retbillet/Testfileuser.java
+++ src/retbillet/Testfileuser.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package retbillet;
-
+import java.io.IOException;
 /**
  *
  * @author phill
@@ -14,10 +14,28 @@ public class Testfileuser {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
+        int errors=0;
+        //test file existing
         System.out.println("expectet output: (Found file: c:/temp/test.txt) or (Success in creating file)");
         Fileuser m = new Fileuser("c:/temp/","test.txt");
         
+        //test write read
+        System.out.println("\nenter a line (Hello world) in test.txt");
+        String e = "Hello World";
+        m.filewrite(e);
+        System.out.println("done now to read it and detect error");
+        m.readfile();
+        
+        //test search with no balance data
+        Fileuser usertest = new Fileuser("c:/temp/","usertest.txt");
+        usertest.filewrite("Dorte,1234,");
+        if(usertest.fileSearchUser("Dorte", "1234") != 1) errors++; //search with valid ID and password
+        if(usertest.fileSearchUser("Hans", "1234") != -1) errors++; //search with invalid ID but valid password
+        if(usertest.fileSearchUser("Dorte", "2233") != 0) errors++; //search with valid ID but invalid password
+        System.out.println("there where "+errors+" errors");
+        
+        //test search with balance data
     }
     
 }
